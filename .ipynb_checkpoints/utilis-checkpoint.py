3f3636deb00725afe84d3d66dd2bc12a525bfead
pandeysudip <sudip@siu.edu>
2021-12-29 19:45:51 -0800

    updating

--- .ipynb_checkpoints/utilis-checkpoint.py
+++ .ipynb_checkpoints/utilis-checkpoint.py
@@ -1,17 +1,16 @@
 import joblib
 import numpy as np
 import pandas as pd
-import test
 from sklearn.preprocessing import StandardScaler
 
 
 def preprocess(variables=['Rating', 'Size', 'Type of ownership', 'Industry',
                           'Sector', 'Revenue', 'State', 'desc_length']):
-    #dummies = pd.get_dummies(variables)
-    #std = StandardScaler()
-    test1 = test.test
+    dummies = pd.get_dummies(variables)
+    std = StandardScaler()
+    test = std.fit_transform(dummies)
 
     trained_model = joblib.load('model.pkl')
-    prediction = trained_model.predict(test1)
+    prediction = trained_model.predict([test])
 
     return prediction
