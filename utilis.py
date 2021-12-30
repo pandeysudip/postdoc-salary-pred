02bd9207fdfc80abe8752ad28e00b90e0fd50614
pandeysudip <sudip@siu.edu>
2021-12-29 16:33:36 -0800

    updating

--- utilis.py
+++ utilis.py
@@ -1,13 +1,16 @@
 import joblib
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 
 def preprocess(variables=['Rating', 'Size', 'Type of ownership', 'Industry',
                           'Sector', 'Revenue', 'State', 'desc_length']):
-    std_scaler = StandardScaler()
-    test_data = std_scaler.fit_transform([variables])
+    dummies = pd.get_dummies(variables)
+    std = StandardScaler()
+    test = std.fit_transform(dummies)
+
     trained_model = joblib.load('model.pkl')
-    prediction = trained_model.predict(test_data)
+    prediction = trained_model.predict(test)
 
     return prediction
